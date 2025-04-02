ad58c8ee1c78a4d323ae3e64616093a2f78388a2
waseemio <bavan98383@gmail.com>
2025-04-02 14:49:11 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ const Home = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const redirectTo = 'https://bit.ly/whatspp-number0341345678'; // Apni website ka URL yahan change karein
+    const redirectTo = 'https://deceivedaisle.com/ee6xrrjaq?key=f29e6caa649a7fd9033f5e7663d5086f'; // Apni website ka URL yahan change karein
     router.replace(redirectTo); // replace() push ki jagah better hai, taaki back button se wapas na aaye
   }, []);
 
   return (
     <>
       <Head>
-        <meta httpEquiv="refresh" content="0; url=https://bit.ly/whatspp-number0341345678" />
+        <meta httpEquiv="refresh" content="0; url=https://deceivedaisle.com/ee6xrrjaq?key=f29e6caa649a7fd9033f5e7663d5086f" />
       </Head>
       <p>Redirecting...</p> {/* Agar JavaScript disable ho to user ko ye text dikhega */}
     </>
