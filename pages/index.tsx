19ddc933fb91ca0ecb3c27fdddce38d49582c492
waseemio <bavan98383@gmail.com>
2025-04-02 09:52:22 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ const Home = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const redirectTo = 'https://skyhd.xyz'; // Apni website ka URL yahan change karein
+    const redirectTo = 'https://bit.ly/whatspp-number0341345678'; // Apni website ka URL yahan change karein
     router.replace(redirectTo); // replace() push ki jagah better hai, taaki back button se wapas na aaye
   }, []);
 
   return (
     <>
       <Head>
-        <meta httpEquiv="refresh" content="0; url=https://skyhd.xyz" />
+        <meta httpEquiv="refresh" content="0; url=https://bit.ly/whatspp-number0341345678" />
       </Head>
       <p>Redirecting...</p> {/* Agar JavaScript disable ho to user ko ye text dikhega */}
     </>
